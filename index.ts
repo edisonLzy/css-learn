a7430b8ed366e699e6b5024716642462f9e2ef54
LEEZHIYU <656603135@qq.com>
2020-08-16 23:56:35 +0800

    feat(grid): 新增grid布局知识
    新增grid布局知识

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 import './src/index.scss';
 import './src/xHeight';
+import './src/grid'
---
+++ src/grid/index.ts
@@ -0,0 +1,6 @@
+import {createItem} from '../utils';
+import './index.scss';
+const h = require('./index.html')
+// ex实现icon居中
+createItem({title:'grid',article:h})
+
