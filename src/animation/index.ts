0f358ca089c775593cd1ab66533f4356df795006
evanzyli <>
2020-11-15 15:42:07 +0800

    feat(animation): 新增 animation

--- src/animation/index.ts
+++ src/animation/index.ts
@@ -1,14 +1,21 @@
+import { log } from 'console';
 import {createItem} from '../utils';
 import './index.scss';
-const h = require('./index.html')
+const h = require('./index.html');
 // ex实现icon居中
-createItem({title:'animation & transition',article:h})
+createItem({title:'animation & transition',article:h});
 
-// 注册 transitionend 事件
-
-const square = document.querySelector("#square");
-
-const handler  = e =>{
-    e.target.innerHTML = 'done';
-}
-square.addEventListener('transitionend',handler)
+// 注册 动画 事件
+(function(){
+	let count = 0;
+	const clock = document.querySelector('.clock');
+	const handler = e=>{
+		console.log('动画执行' + count++);
+		if(count === 5){
+			console.log(e);
+			console.log('动画结束' + count);
+			clock.removeEventListener('animationiteration',handler);
+		}
+	};
+	clock.addEventListener('animationiteration',handler);
+})();
