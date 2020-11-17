08b7d5d47b4fb79870e89325fae71450f8e108c2
evanzyli <>
2020-11-17 14:09:25 +0800

    feat(animation): 更新 animation

--- src/animation/index.ts
+++ src/animation/index.ts
@@ -19,3 +19,38 @@ createItem({title:'animation & transition',article:h});
 	};
 	clock.addEventListener('animationiteration',handler);
 })();
+
+
+// web animations Api
+(function(){
+	const target:HTMLElement = document.querySelector('.animate');
+	target.addEventListener('click',e=>{
+		const player = target.animate([
+			{transform: 'translate(0)'},
+			{transform: 'translate(100px, 100px)'}
+		],{
+			duration:500,
+			fill:'forwards'
+		});
+	});
+})();
+
+
+// 视图间的动画切换
+(function(){
+	function toggle(){
+		container.classList.toggle('view-change');
+	}
+
+	const toDetailBtn = document.querySelector('#toDetail'),
+		toListBtn = document.querySelector('#toList'),
+		showModel=document.querySelector('#showModel'),
+		view = document.querySelector('.view'),
+		container = document.querySelector('.toggle');
+	showModel.addEventListener('click',function(){
+		view.classList.toggle('visible');
+	});
+	toListBtn.addEventListener('click',toggle);
+	toDetailBtn.addEventListener('click',toggle);
+})();
+
